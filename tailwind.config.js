68acf33e49c1f823b8ed7d0f46a8b18031ddf4c6
swathi <swathimaddipatla11@gmail.com>
2024-09-20 09:33:11 +0530

    committing

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+
+        fontFamily:{
+            Outfit: ["Outfit", "sans-serif"],
+            Ovo: ["Ovo", "serif"]
+        },
+         colors:{
+            darkTheme: '#11001F'
+         },
+         darkMode: 'selector'
+        }
+    
+            
+        
+        
+    
